7b1eb96268cb6822cf70c425f440e2844ff05024
TyomaIsaev <161722195+TyomaIsaev@users.noreply.github.com>
2024-03-07 14:50:02 +0300

    graphics

---
+++ python/newpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([100,10100,20100,30100,40100,50100,60100])
+y = np.array([14.9,0.7,1703.6,5674.3,0.1,1090.8,1587.7])
+plt.plot(x, y)
+plt.show()
